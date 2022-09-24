c2cf67c1ae8a8047ccc3be166e7dff2fc16db557
edigonzales <edi.gonzales@gmail.com>
2022-09-24 17:21:12 +0200

    forward filter already existed

--- src/main/java/ch/so/agi/gb2av/Gb2avApplication.java
+++ src/main/java/ch/so/agi/gb2av/Gb2avApplication.java
@@ -2,8 +2,6 @@ package ch.so.agi.gb2av;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class Gb2avApplication {
@@ -11,9 +9,4 @@ public class Gb2avApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Gb2avApplication.class, args);
 	}
-
-    @Bean
-    ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
-    } 
 }
